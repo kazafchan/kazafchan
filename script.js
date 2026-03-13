ff36be0ffb24d6a12cfba40abcbd6ee42a38ff97
kazafchan <kazafchancy@gmail.com>
2026-03-13 17:36:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,25 @@ function calculateQuickDash() {
         alert(`Please answer at least 10 questions. (Currently: ${count})`);
     }
 }
+function calculateMHQ() {
+    // Function Domain Logic (Questions mhq1, etc.)
+    const funcOptions = document.querySelectorAll('input[name^="mhq1"]:checked');
+    if (funcOptions.length > 0) {
+        let sum = 0;
+        funcOptions.forEach(opt => sum += parseInt(opt.value));
+        // Normalizing to 0-100: ((Sum - Min) / (Max - Min)) * 100
+        // For MHQ, usually lower raw is better, so we flip it
+        let score = ((5 - (sum / funcOptions.length)) / 4) * 100;
+        document.getElementById('score-function').innerText = score.toFixed(1) + "%";
+    }
+
+    // Pain Domain Logic (Questions mhq_pain1, etc.)
+    const painOptions = document.querySelectorAll('input[name^="mhq_pain"]:checked');
+    if (painOptions.length > 0) {
+        let sum = 0;
+        painOptions.forEach(opt => sum += parseInt(opt.value));
+        // In Pain domain, 1 is "no pain". To make 100 = "no pain":
+        let score = ((5 - (sum / painOptions.length)) / 4) * 100;
+        document.getElementById('score-pain').innerText = score.toFixed(1) + "%";
+    }
+}
